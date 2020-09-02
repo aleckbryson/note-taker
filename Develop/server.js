eb8baec9416f25fc76b7ea382a07280e53be2f95
Aleck Tuluenga <aleckbryson@gmail.com>
2020-09-02 19:42:55 -0400

    Routes added to get the index and notes.html

--- Develop/server.js
+++ Develop/server.js
@@ -12,3 +12,17 @@ var PORT = process.env.PORT || 4000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes added for notes.html and index.html files
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"));
+});
+
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "./notes.html"));
+});
+
+// read the db.json file and return all saved notes as JSON.
+app.get("/api/notes", function(req, res) {
+    res.json(notes);
+  });
+
